ce0ef224e431344c32192c7dbbc3d1c50765c547
omnia atef <omnia.atef@add.int>
2019-05-21 12:16:01 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule,
+         MatSelectModule, 
+         MatInputModule, 
+         MatButtonModule 
+        } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
